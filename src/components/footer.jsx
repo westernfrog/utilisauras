d43395a0c07d327796d4437dcf2ef0ffe1f85a63
Aman Singh <haaamansingh007@gmail.com>
2022-10-03 17:30:41 +0530

    Updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+let cdate = new Date();
+cdate = cdate.getFullYear();
+
 export default function Footer() {
   return (
     <div className="container">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top container-fluid">
-        <p className="col-md-4 mb-0">&copy; 2021 Text Utility, Inc</p>
+        <p className="col-md-4 mb-0">&copy; {cdate} Online Utility, Inc</p>
 
         <img
           src="https://cdn-icons-png.flaticon.com/512/2087/2087807.png"
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,98 +3,66 @@ import React from "react";
 export default function Header() {
   return (
     <div className="bg-dark">
-      <nav className="navbar navbar-expand-lg navbar-dark container">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Text Utility
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <ul className="navbar-nav  mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Home
+      <nav class="navbar navbar-dark pt-3 rounded container">
+        <div class="container-fluid">
+          <figure className="mb-0 pb-0 text-light">
+            <blockquote class="blockquote">
+              <h1>Online Utility</h1>
+            </blockquote>
+          </figure>
+        </div>
+      </nav>
+      <div className="bg-light shadow-sm">
+        <nav class="navbar navbar-expand-lg navbar-light container">
+          <div class="container-fluid">
+            <span class="navbar-brand">Utilities</span>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              style={{
+                boxShadow: "none",
+                border: "none",
+              }}
+            >
+              <span class="material-symbols-outlined">settings</span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="true" href="/">
+                    PDF TO WORD
                   </a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    About
+                <li class="nav-item ">
+                  <a class="nav-link" href="/">
+                    WORD TO PDF
                   </a>
                 </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="/"
-                    id="navbarDarkDropdownMenuLink"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Features
+                <li class="nav-item ">
+                  <a class="nav-link" href="/">
+                    TIME CONVERSIONS
                   </a>
-                  <ul className="dropdown-menu dropdown-menu-dark ">
-                    <li>
-                      <a className="dropdown-item" href="/">
-                        JSON Formatters
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-
-                    <li>
-                      <a className="dropdown-item" href="/">
-                        Time Conversions
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="/">
-                        Online Text tools
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-
-                    <li>
-                      <a className="dropdown-item" href="/">
-                        Online Hex and Binary tools
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-
-                    <li>
-                      <a className="dropdown-item disabled" href="/l">
-                        <h5>
-                          <span className="badge bg-secondary">
-                            More content coming soon!
-                          </span>
-                        </h5>
-                      </a>
-                    </li>
-                  </ul>
+                </li>
+                <li class="nav-item ">
+                  <a class="nav-link" href="/">
+                    TEXT UTILITY
+                  </a>
+                </li>
+                <li class="nav-item ">
+                  <a class="nav-link" href="/">
+                    JSON FORMATTERS
+                  </a>
                 </li>
               </ul>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 }
