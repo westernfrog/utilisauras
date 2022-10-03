865b29032d66bbabef29a0648df119fa666164f6
Aman Singh <haaamansingh007@gmail.com>
2022-10-03 19:50:13 +0530

    Updated

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 function Time() {
   setInterval(updateTime, 1000);
   const now = new Date().toLocaleTimeString();
+  const date = new Date();
+  const [day, setday] = useState(" ");
+
   const [time, settime] = useState(now);
 
   function updateTime() {
@@ -11,9 +14,10 @@ function Time() {
   }
 
   return (
-    <div className="container">
+    <div className="container my-3">
       <button className="btn">
         <h1>{time}</h1>
+        <h1>{day}</h1>
       </button>
     </div>
   );
